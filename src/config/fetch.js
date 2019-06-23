8cb3ddeac2fb9d9c559dbe7570e564e5c17f6cbe
yezi <870349413@qq.com>
2019-06-23 22:14:01 +0800

    添加请求loading

--- src/config/fetch.js
+++ src/config/fetch.js
@@ -1,4 +1,5 @@
-import Qs from 'qs';
+import React from 'react';
+import { Toast } from 'antd-mobile-rn';
 //一个 Promise 就是一个代表了异步操作最终完成或者失败的对象
 const baseURL = 'http://106.14.155.124/api/';
 export default class HttpUtils {
@@ -16,7 +17,12 @@ export default class HttpUtils {
         )
     };
     static post = (url, data) => {
-        console.log(url, data, '---------------------------')
+        Toast.loading('请稍等...', 0);
+        let params = {
+            user_id: global.userInfo && global.userInfo.id,
+            ...data
+        }
+        console.log('--------------', url, params, '---------------------------')
         return new Promise(((resolve, reject) => {
             fetch(baseURL + url, {
                 method: 'POST',
@@ -24,14 +30,16 @@ export default class HttpUtils {
                     'Accept': 'application/json',//告诉服务器，我们能接受json格式的返回类型，
                     'Content-Type': 'application/json;charset=utf-8' //告诉服务器，我们提交的数据类型
                 },
-                body: JSON.stringify(data),//(把你想提交得数据序列化为json字符串类型，然后提交)body中的数据就是我们需要向服务器提交的数据,比如用户名,密码等
+                body: JSON.stringify(params),//(把你想提交得数据序列化为json字符串类型，然后提交)body中的数据就是我们需要向服务器提交的数据,比如用户名,密码等
             })//返回 服务器处理的结果
                 .then(response => response.json())
                 .then(result => {
+                    Toast.hide();
                     console.log(result, 'resultresult')
                     resolve(result);
                 })
                 .catch(error => {
+                    Toast.hide();
                     reject(error);
                 })
         })
